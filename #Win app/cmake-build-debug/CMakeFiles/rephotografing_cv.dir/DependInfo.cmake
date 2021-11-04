92d2e87e409b8997062dd1f273355a2132525c64
Tomáš Ryšavý <xrysav27@vutbr.cz>
2021-11-04 15:41:26 +0100

    add Win app

---
+++ #Win app/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Refotografing-source/src/CameraCalibrator.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/CameraCalibrator.cpp.obj"
+  "D:/Refotografing-source/src/Line.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/Line.cpp.obj"
+  "D:/Refotografing-source/src/MSAC.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/MSAC.cpp.obj"
+  "D:/Refotografing-source/src/Main.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/Main.cpp.obj"
+  "D:/Refotografing-source/src/ModelRegistration.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/ModelRegistration.cpp.obj"
+  "D:/Refotografing-source/src/PnPProblem.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/PnPProblem.cpp.obj"
+  "D:/Refotografing-source/src/RobustMatcher.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/RobustMatcher.cpp.obj"
+  "D:/Refotografing-source/src/Utils.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/Utils.cpp.obj"
+  "D:/Refotografing-source/src/errorNIETO.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/errorNIETO.cpp.obj"
+  "D:/Refotografing-source/src/lmmin.cpp" "D:/Refotografing-source/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/src/lmmin.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/OpenCV/opencv/build/install/include"
+  "D:/OpenCV/opencv/build/install/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ #Win app/cmake-build-debug/CMakeFiles/rephotografing_cv.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rephotografing_cv.dir/src/Main.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/ModelRegistration.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/PnPProblem.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/RobustMatcher.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/Utils.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/CameraCalibrator.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/Line.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/MSAC.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/errorNIETO.cpp.obj"
+  "CMakeFiles/rephotografing_cv.dir/src/lmmin.cpp.obj"
+  "rephotografing_cv.pdb"
+  "rephotografing_cv.exe"
+  "rephotografing_cv.exe.manifest"
+  "librephotografing_cv.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rephotografing_cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
