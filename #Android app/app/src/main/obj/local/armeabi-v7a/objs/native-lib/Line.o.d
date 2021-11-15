635007687171dd34f8294808d52e7398315f5969
Tomáš Ryšavý <xrysav27@vutbr.cz>
2021-11-15 13:24:59 +0100

    delete app

--- #Android app/app/src/main/obj/local/armeabi-v7a/objs/native-lib/Line.o.d
+++
@@ -1,5 +0,0 @@
-D:\Refotografing-native\app\src\main/obj/local/armeabi-v7a/objs/native-lib/Line.o: \
-  D:/Refotografing-native/app/src/main/jni/Line.cpp \
-  D:/Refotografing-native/app/src/main/jni/Line.h
-
-D:/Refotografing-native/app/src/main/jni/Line.h:
